34bf113750f36e1b44d93b1d721527384ceda72f
Kento Tsumuraya <sauzar18@gmail.com>
2019-05-29 01:54:18 +0900

    mysql information commit

--- server/mysqlConnect.js
+++ server/mysqlConnect.js
@@ -1,10 +1,16 @@
 import mysql from 'mysql'
 
 const dbConfig = {
+  /*
   host: 'localhost',
   user: 'root',
   database: 'dev_db',
   password: '',
+  */
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b1038310007f5e',
+  database: 'heroku_1e39c00e23ca36c',
+  password: '4cf9784b',
   multipleStatements: true
 }
 const connection = mysql.createConnection(dbConfig)
